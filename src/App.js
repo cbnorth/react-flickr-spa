33c533b73a215b8afc9d07f88b301f7455877e57
Paul Miller <burke.cf@gmail.com>
2016-10-10 14:58:56 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import Slider from 'react-slick';
+
+
+
+let App = React.createClass ({
+
+  getInitialState() {
+    return {
+      data: [],
+      photos: ["loading"]
+    }
+  },//state
+  getPhotos() {
+    axios.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6d169abba1c0afe3002ea6c9a5549924&tags=iceland&accuracy=62.00%2C+64.00%2C+18.00%2C+21.00&per_page=50&format=json&nojsoncallback=1&auth_token=72157673746422511-9255a2ac10975a54&api_sig=d92d88f49dff674c923c39b531142fe6')
+      .then((response) =>{
+        this.setState({
+          data: response.data.photos.photo,
+          photos: []
+        })
+      });
+  },
+
+  componentDidMount() {
+    this.getPhotos();
+  },
+
+  render() {
+
+    for (let key in this.state.data) {
+      if(this.state.data.hasOwnProperty(key)) {
+        let farm = this.state.data[key].farm;
+        let serverID = this.state.data[key].server;
+        let photoID = this.state.data[key].id;
+        let secret = this.state.data[key].secret;
+        let title = this.state.data[key].title;
+        this.state.photos.push(<img src={`https://farm${farm}.staticflickr.com/${serverID}/${photoID}_${secret}_b.jpg`} key={key} data-title={`${title}`} alt={`${title}`}/>);
+      }
+    };
+
+
+    var items = this.state.photos.map(function (photo, index) {
+        return (<div key={index}>{photo}</div>);
+    });
+
+
+    let settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+
+
+    return (
+      <div className='container'>
+        <Slider {...settings}>{items}</Slider>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
