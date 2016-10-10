bf24c43dd6bea17cf8604fde48c0f1b113ee8d91
Paul Miller <burke.cf@gmail.com>
2016-10-10 16:41:12 -0700

    polished styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import Slider from 'react-slick';
-
+import './styles/Styles.css';
 
 
 let App = React.createClass ({
@@ -12,7 +12,8 @@ let App = React.createClass ({
       data: [],
       photos: ["loading"]
     }
-  },//state
+  },
+
   getPhotos() {
     axios.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6d169abba1c0afe3002ea6c9a5549924&tags=iceland&accuracy=62.00%2C+64.00%2C+18.00%2C+21.00&per_page=50&format=json&nojsoncallback=1&auth_token=72157673746422511-9255a2ac10975a54&api_sig=d92d88f49dff674c923c39b531142fe6')
       .then((response) =>{
@@ -20,6 +21,9 @@ let App = React.createClass ({
           data: response.data.photos.photo,
           photos: []
         })
+      })
+      .catch(function (error) {
+        console.log('there was an error');
       });
   },
 
@@ -57,6 +61,7 @@ let App = React.createClass ({
 
     return (
       <div className='container'>
+        <h1>React Flickr Slideshow</h1>
         <Slider {...settings}>{items}</Slider>
       </div>
     );
